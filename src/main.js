709a53d17b734fbd8f8493f828b714968868cb5a
alexei suzdalenko <alexei.saron@gmail.com>
2021-09-11 17:06:36 +0200

    asdf asdf

--- src/main.js
+++ src/main.js
@@ -8,3 +8,13 @@ createApp(App).use(store).use(router).mount('#app')
 
 // https://www.ekko-wp.com/moving/
 // feed antique light feed axis pact armed cereal limit close glow sunny
+// https://moling.dan-fisher.com/index.html
+// https://html.modernwebtemplates.com/moveco/
+// https://html.modernwebtemplates.com/mover/index.html
+
+// https://templatebundle.net/templates/mover-and-packers-html-template/
+
+// other theme https://themewagon.com/
+// https://technext.github.io/yavin/index.html#header 
+// https://technext.github.io/nubis/index.html
+// vue js template
